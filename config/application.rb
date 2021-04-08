0d8a3277268721a6b860a12b465cd2cbe5290e41
Laura Travin <laura.travin@gmail.com>
2021-04-07 20:48:15 -0400

    Smaill test update

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ Bundler.require(*Rails.groups)
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+     config.action_controller.permit_all_parameters = false  #this is not working at all.
     end
 end
